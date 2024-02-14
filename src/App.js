543ea8ae8174a35cd54837a00f84d362b79a8d49
Patryk Sawicki <sawicki.patryk129@gmail.com>
2024-02-14 15:12:42 +0100

    cleard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react'
 
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/Header/Header'
-
 export const App = () => {
   return (
     <>
-      <Navbar/>
-      <Header/>
     </>
   )
 }
